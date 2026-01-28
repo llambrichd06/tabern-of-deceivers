6333922c8315c5cf889d9b17d5bc957f2be1850a
llambrichd06 <llambrichd06@gmail.com>
2026-01-28 20:37:07 +0100

    whatever

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index() {
-        $posts = Post::all();
+        $posts = Post::paginate(2);
         return $posts;
     }
 
