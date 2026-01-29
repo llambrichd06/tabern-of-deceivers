7448472aaee43b3376f25be63173e603e560a963
miquelfontdevilafp-oss <miquelfontdevilafp@ibf.cat>
2026-01-29 16:32:41 +0100

    TOD-2 function show and destroy leaderbord controller.

--- app/Http/Controllers/Api/LeaderboardController.php
+++ app/Http/Controllers/Api/LeaderboardController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Leaderboard;
 use Illuminate\Http\Request;
 
 class LeaderboardController extends Controller
 {
     public function index() {
         
+    }
+    public function show(Leaderboard $leaderboard) {
+        return $leaderboard;
+    }
+    public function destroy(Leaderboard $leaderboard) {
+        $leaderboard->delete();
+        return "deleted successfully";
     }
 }
