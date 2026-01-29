011e50c46caf9c6797c2e174a8cd20c82d159915
miquelfontdevilafp-oss <miquelfontdevilafp@ibf.cat>
2026-01-29 16:15:08 +0100

    TOD-2  leaderboard migration made

--- database/migrations/2026_01_29_150625_create_leaderboards_table.php
+++ database/migrations/2026_01_29_150625_create_leaderboards_table.php
@@ -13,7 +13,14 @@ return new class extends Migration
     {
         Schema::create('leaderboards', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->integer('points');
+            $table->integer('wins');
+            $table->integer('losses');
+            $table->integer('matches');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
