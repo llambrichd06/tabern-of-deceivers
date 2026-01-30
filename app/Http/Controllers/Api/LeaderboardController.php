e0a1c959a5df11772300f8389d2a8034eec79a65
llambrichd06 <llambrichd06@gmail.com>
2026-01-30 20:02:53 +0100

    TOD-2 made store and update calls for leaderboards

--- app/Http/Controllers/Api/LeaderboardController.php
+++ app/Http/Controllers/Api/LeaderboardController.php
@@ -42,6 +42,43 @@ class LeaderboardController extends Controller
     public function show(Leaderboard $leaderboard) {
         return $leaderboard;
     }
+    
+    public function store(Request $request) {
+        $data = $request->validate([
+            'user_id' => ['required', 'exists:users,id'],
+            'points' => ['nullable', 'min:0'],
+            'wins' => ['required', 'min:0'],
+            'losses' => ['required', 'min:0'],
+            'matches' => ['required', 'min:0'],
+        ]);
+
+        
+        $leaderboard = Leaderboard::create($data);
+        return $leaderboard;
+        
+    }
+
+    public function update(Request $request, Leaderboard $leaderboard) {
+        $leaderboard = Leaderboard::find($leaderboard->id);
+        $data = $request->validate([
+            'user_id' => ['nullable', 'exists:users,id'],
+            'points' => ['nullable', 'min:0'],
+            'wins' => ['nullable', 'min:0'],
+            'losses' => ['nullable', 'min:0'],
+            'matches' => ['nullable', 'min:0'],
+        ]);
+        
+        $leaderboard->user_id = $data->user_id ?? $leaderboard->user_id;
+        $leaderboard->points = $data->points ?? $leaderboard->points;
+        $leaderboard->wins = $data->wins ?? $leaderboard->wins;
+        $leaderboard->losses = $data->losses ?? $leaderboard->losses;
+        $leaderboard->matches = $data->matches ?? $leaderboard->matches;
+        
+        $leaderboard->save();
+        return $leaderboard;
+        
+    }
+
     public function destroy(Leaderboard $leaderboard) {
         $leaderboard->delete();
         return "deleted successfully";
