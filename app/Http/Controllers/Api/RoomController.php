146260d176cc659deb278f792f58e67700182a44
miquelfontdevilafp-oss <miquelfontdevilafp@ibf.cat>
2026-02-06 18:40:27 +0100

    TOD-2-api-room-controller

--- app/Http/Controllers/Api/RoomController.php
+++ app/Http/Controllers/Api/RoomController.php
@@ -16,4 +16,35 @@ class RoomController extends Controller
     public function show(Room $room) {
         return $room;
     }
+    
+    // public function kickPlayer() //idea a futur
+    
+    public function store(Request $request) {
+        $data = $request->validate([
+            'room_code' => ['required', 'min:8', 'max:8'],
+            'state' => ['required', 'in:lobby, on_going, completed']
+        ]);
+        $room = Room::create($data);
+        return $room;
+    }
+
+    public function update(Request $request, Room $room) {
+        $room = Room::find($room->id);
+        $data = $request->validate([
+            'room_code' => ['required', 'size:8'],
+            'state' => ['required', 'in:lobby, on_going, completed']
+        ]);
+     
+        $room->room_code = $data->room_code ?? $room->room_code;
+        $room->state = $data->state ?? $room->state;
+     
+        $room->save();
+        return $room;
+    }
+
+    public function destroy(Room $room) {
+        $room->delete();
+        return "deleted successfully";
+    }
 }
+
--- app/Models/Room.php
+++ app/Models/Room.php
@@ -8,11 +8,6 @@ class Room extends Model
 {
     protected $fillable = [
         'room_code', //Code for the users to join the room
-        'player_1',  //Users that are able to join the room
-        'player_2',
-        'player_3',
-        'player_4',
-        'player_5',
-        'player_6',
+        'state', //3 states lobby, on_going, completed
     ];
 }
