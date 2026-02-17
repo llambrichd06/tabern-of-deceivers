c802fe49fb45cae7d14369d1342a88d52f3f2b4c
llambrichd06 <llambrichd06@gmail.com>
2026-02-17 18:13:20 +0100

    TOD-3 Created view skeleton for match result and view room components
    added a class on the global css for the required icon color

---
+++ resources/js/components/matchComponents/MatchResult.vue
@@ -0,0 +1,35 @@
+<template>
+    <dialog>
+        <h1></h1>
+        <div>
+            <div> <!-- Only putting one of each value because we'll loop it later somehow. -->
+                <h2>Position</h2>
+                <div>
+                    <p v-for="i in 6">
+                        {{ n }}
+                    </p>
+                </div>
+            </div>
+            <div>
+                <h2>Players</h2>
+                <div>
+                    <Image src="" alt="Profile Picture"/>
+                    <p>userName</p>
+                </div>
+            </div>
+            <div>
+                <h2>Cards left</h2>
+                <p></p>
+            </div>
+        </div>
+    </dialog>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- resources/js/components/roomComponents/JoinRoom.vue
+++ resources/js/components/roomComponents/JoinRoom.vue
@@ -1,7 +1,22 @@
 <template>
     <dialog>
         <h1></h1>
-        
+        <div>
+            <form action="">
+                <FloatLabel variant="on"> 
+                    <InputText id="code" v-model="code" />
+                    <label for="code">Room Code: <span class="requiredIcon">*</span></label>
+                </FloatLabel>
+                <FloatLabel variant="on">
+                    <InputText id="password" v-model="password" />
+                    <Message size="small" severity="secondary" variant="simple">If the room has no password, leave this empty.</Message>
+                    <label for="password">Password</label>
+                </FloatLabel>
+                <Button label="Enter"/>
+                <Button label="Close"/>
+            </form>
+        </div>
+
     </dialog>
 </template>
 
@@ -14,3 +29,4 @@ import { ref } from 'vue';
 <style lang="scss" scoped>
 
 </style>
+
