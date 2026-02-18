d5140643981683315381a4390edbbf6421fd70c8
miquelfontdevilafp-oss <miquelfontdevilafp@ibf.cat>
2026-02-18 18:50:34 +0100

    TOD-4 Creacio arxius rankings i rooms

---
+++ resources/js/composables/rankings.js
@@ -0,0 +1,16 @@
+import { ref, inject } from 'vue'
+import { useRouter } from 'vue-router'
+import axios from 'axios'
+import * as yup from 'yup'
+import { useValidation } from './useValidation'
+import { useToast } from './useToast'
+
+export default function usePosts() {
+    const { errors, validate, clearErrors, hasError, getError } = useValidation()
+    const router = useRouter()
+    const toast = useToast()
+
+    const isLoading = ref(false)
+
+    
+}
