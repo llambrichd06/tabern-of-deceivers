4cd90953746ca78cb9b23f0fc75eb985bfe27b66
llambrichd06 <llambrichd06@gmail.com>
2026-04-08 20:42:27 +0200

    TOD-8 Cards played functions and event

--- app/Http/Controllers/Api/GameController.php
+++ app/Http/Controllers/Api/GameController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Events\CardPlayed;
 use App\Events\StartGame;
 use App\Events\UpdateGameState;
 use App\Events\GameWon;
@@ -29,8 +30,6 @@ class GameController extends Controller
     {
         $game = Game::find($request->id);
 
-
-
         return response()->json(['game_state' => $game->game_state]);
     }
 
@@ -41,7 +40,7 @@ class GameController extends Controller
         $room_id = $request->room_id;
         $gameForRoomExists = Game::where('room_id', $room_id)->where('is_finished', '0');
 
-        if ($gameForRoomExists) return response()->json(['error' => 'This room has an unfinished game']);
+        if ($gameForRoomExists) return response()->json(['error' => 'This room has an unfinished game'], 409);
 
         $room = Room::where('room_id', $room_id)->first();
         $room->state = 'on_going';
@@ -53,16 +52,13 @@ class GameController extends Controller
         $game->game_state = $this->createNewGameState($room_id);
         $game->save();
 
-
-
         broadcast(new StartGame($game));
-        // return response()->json(['game_state' => $game->game_state]);
+        $this->updateGameState($game->id, $game->game_state);
     }
 
 
-
-    private function createNewGameState($room_id)
-    {
+    private function createNewGameState($room_id) {
+        //number of cards per player
         $cardsPerPlayer = 6;
         $gameState = new stdClass;
 
@@ -84,28 +80,35 @@ class GameController extends Controller
 
         $gameState->players = [];
 
-
         $gameState->current_player_turn = 0;
+        $gameState->last_player_turn = 0;
         $gameState->turn = 0;
 
+        //setting the players in a random order
         $players = RoomUsers::select('user_id')->where('room_id')->inRandomOrder()->get();
         foreach ($players as $player) {
             array_push($gameState->players, $player->id);
         }
+        
+        //depending on player number, how many cards do we need to use in the game
+        $amountOfCards = count($gameState->players)*$cardsPerPlayer;
 
-        $amountOfCards = count($gameState->players) * $cardsPerPlayer;
-
+        //grab the amount of cards needed randomly
         $cards = Card::inRandomOrder()->limit($amountOfCards)->get();
 
-        $playerNum = 0;
+        $playerNum = 1;
         foreach ($cards as $card) {
+            //update card count for current player
             $gameState->player_decks["player$playerNum"]['count'] += 1;
+            //put the card in the player's deck
             array_push($gameState->player_decks["player$playerNum"]['cards'], $card);
+            //if the amount of cards in the deck is equal (or greater, somehow) to the setted amount of cards per player, give cards to the next player
             if (count($gameState->player_decks["player$playerNum"]['cards']) >= $cardsPerPlayer) {
                 $playerNum++;
             }
         }
 
+        //encode the game state to json to save it in the DB
         $jsonGameState = json_encode($gameState);
 
         return $jsonGameState;
@@ -123,14 +126,77 @@ class GameController extends Controller
         }
     }
 
-    private function validAction() {
+    private function validateAction() {
 
     }
+
+    public function playCards(Request $request) {
+        $gameState = $this->getDecodedGameStateById($request->gameId);
+        $playerNum = $this->getLoggedPlayerNumFromState($gameState);
+        $cardsPlayed = $request->idCards;
+        $lastCalledRank = $request->calledRank;
+        
+        try {
+            //Check if the game is finished
+            $isGameFinished = $this->changeTurn($request->gameId, $playerNum);
+            if ($isGameFinished) {
+                return;
+            }
+
+            //if there was no called rank
+            if ($gameState->pile['called_rank'] <= 0) {
+               $gameState->pile['called_rank'] = $lastCalledRank;
+            }
+
+            $gameState->pile['last_played_cards'] = $cardsPlayed;
+
+            //for each card played we do the same
+            foreach ($cardsPlayed as $cardId) {
+                //look for the card position in the array
+                $cardPosition = array_search($cardId, $gameState->player_decks["player$playerNum"]['cards']);
+                //remove the card from the player's deck
+                unset($gameState->player_decks["player$playerNum"]['cards'][$cardPosition]);
+                //add the cards to the pile
+                array_push($gameState->pile['cards'], $cardId);
+            }
+            //change the card count of the player to the new card count
+            $gameState->player_decks["player$playerNum"]['count'] = count($gameState->player_decks["player$playerNum"]['cards']);
+            //change the pile count to the new count
+            $gameState->pile['count'] = count($gameState->pile['cards']);
+            
+            $jsonGameState = json_encode($gameState);
+
+            $this->updateGameState($request->gameId, $jsonGameState);
+            broadcast(new CardPlayed(Game::find($request->gameId)));
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    private function changeTurn($gameId, $playerNum) {
+        $gameState = $this->getDecodedGameStateById($gameId);
+        $lastPlayerTurn = $gameState->last_player_turn;
+
+        if ($gameState->player_decks["player$lastPlayerTurn"]["count"] >= 0) {
+            $this->gameWon($gameId);
+            return true;
+        }
+        $gameState->last_player_turn = $gameState->current_player_turn;
+        $gameState->current_player_turn = $playerNum;
+        $gameState->turn = $gameState->turn + 1;
+        return false;
+    }
     
-    private function getDecodedGameStateById($gameId) {
+    private function getDecodedGameStateById($gameId) : stdClass {
         $gameState = Game::find($gameId)->game_state;
         $decodedGameState = json_decode($gameState);
         return $decodedGameState;
+    }
+
+    private function getLoggedPlayerNumFromState(stdClass $gameState) : int {
+        $user = Auth::user();
+        $playerNum = array_search($user->id, $gameState->players)+1;
+        return $playerNum;
     }
 
     private function takeCards($gameId, $idPlayerTaker) {
@@ -170,26 +236,18 @@ class GameController extends Controller
         } else {
             return "Error at tring to take cards";
         }
-        
     }
 
     public function callLie($gameId) {
         $user = Auth::user();
         $gameState = $this->getDecodedGameStateById($gameId);
-        
 
         $players = $gameState->players;
-        $totalPlayers = count($players);
-        $currentPlayerTurn = $gameState->current_player_turn;
         $lastPlayedCards = $gameState->last_played_cards;
         $calledRank = $gameState->called_rank;
 
-        $positionOfCurrentPlayer = array_search($currentPlayerTurn, $players);
-        if ($positionOfCurrentPlayer == 0) {
-            $positionOfLastPlayerTurn = $totalPlayers - 1;
-        }
-        $positionOfLastPlayerTurn = $positionOfCurrentPlayer - 1;
-        $lastPlayer = $players[$positionOfLastPlayerTurn];
+
+        $lastPlayer = $players[$gameState->last_player_turn-1];
 
         $lie = false;
         foreach ($lastPlayedCards as $card) {
@@ -200,10 +258,12 @@ class GameController extends Controller
             }
         }
 
-        if ($lie == false) {
-            $gameState = $this->takeCards($gameId, $user);
-        } else {
+        if ($lie == true) {
             $gameState = $this->takeCards($gameId, $lastPlayer);
+            $this->changeTurn($gameId, $user);
+        } else {
+            $gameState = $this->takeCards($gameId, $user);
+            $this->changeTurn($gameId, $lastPlayer);
         } //encara falta la funcio per canviar de torn, un cop creat hem de possar que depenen de si es mentida o no començi el que no roba cartes d'aquesta funcio (si manteix el que ha delatat, si no el que della la veritat)
     }
 
