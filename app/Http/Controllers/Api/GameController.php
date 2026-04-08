2a82d7da7d7b4c45868399cef1653f7ce4691bd8
miquelfontdevilafp-oss <miquelfontdevilafp@ibf.cat>
2026-04-08 17:52:47 +0200

    TOD-8 Funcions de Joc updateGameState, takeCards and callLie
    la funcio updateGameState fa update al estat del joc actual amb la id del joc i el nou estat.
    takeCards es una funcio el qual li dones la id del joc i la id del jugador que robi les cartes de la pila i canvia el estat a que la pila estigui buida i que el jugador rep les cartes.
    callLie es la funcio de dir que algu manteix al joc.

--- app/Http/Controllers/Api/GameController.php
+++ app/Http/Controllers/Api/GameController.php
@@ -9,11 +9,13 @@ use App\Models\Game;
 use App\Models\Room;
 use App\Models\RoomUsers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use stdClass;
 
 class GameController extends Controller
 {
-    public function getUserGameStateById(Request $request) {
+    public function getUserGameStateById(Request $request)
+    {
         $game = Game::find($request->game_id);
 
         if (!$game) return response()->json(['error' => 'Game not found'], 404);
@@ -21,17 +23,19 @@ class GameController extends Controller
         return response()->json(['game_state' => $game->game_state]); //WE NEED TO EDIT THE DATA SENT TO THE USER SO ITS FOR THAT USER SPECIFICALLY
     }
 
-    public function saveGameStateById(Request $request) {
+    public function saveGameStateById(Request $request)
+    {
         $game = Game::find($request->id);
-        
 
-        
+
+
         return response()->json(['game_state' => $game->game_state]);
     }
 
 
 
-    public function create(Request $request) {
+    public function create(Request $request)
+    {
         $room_id = $request->room_id;
         $gameForRoomExists = Game::where('room_id', $room_id)->where('is_finished', '0');
 
@@ -52,10 +56,11 @@ class GameController extends Controller
         broadcast(new StartGame($game));
         // return response()->json(['game_state' => $game->game_state]);
     }
-    
 
 
-    private function createNewGameState($room_id) {
+
+    private function createNewGameState($room_id)
+    {
         $cardsPerPlayer = 6;
         $gameState = new stdClass;
 
@@ -74,10 +79,10 @@ class GameController extends Controller
             'player5' => ['count' => 0, 'cards' => []],
             'player6' => ['count' => 0, 'cards' => []],
         ];
-    
+
         $gameState->players = [];
-        
-        
+
+
         $gameState->current_player_turn = 0;
         $gameState->turn = 0;
 
@@ -86,7 +91,7 @@ class GameController extends Controller
             array_push($gameState->players, $player->id);
         }
 
-        $amountOfCards = count($gameState->players)*$cardsPerPlayer;
+        $amountOfCards = count($gameState->players) * $cardsPerPlayer;
 
         $cards = Card::inRandomOrder()->limit($amountOfCards)->get();
 
@@ -107,36 +112,123 @@ class GameController extends Controller
     private function validAction() {
 
     }
+    
     private function getDecodedGameStateById($gameId) {
         $gameState = Game::find($gameId)->game_state;
         $decodedGameState = json_decode($gameState);
         return $decodedGameState;
     }
 
-}
+    private function updateGameState($gameId, $gameState) {
+        $game = Game::find($gameId);
+        if (!$game) {
+            $game->gameState = $gameState;
+            $game->save();
+            return "Succes in updating the gameState";
+        } else {
+            return "Unsuccesfull to update gameState";
+        }
+    }
 
+    private function takeCards($gameId, $idPlayerTaker) {
+        $gameState = $this->getDecodedGameStateById($gameId);
 
-/*
-{
+        $players = $gameState->players;
+
+        $PositionPlayerTaker = array_search($idPlayerTaker, $players); //rep la posicio del jugador amb id idPlayerTaker de la array players
+        $playerNum = $PositionPlayerTaker + 1;
+        $playerTaker = $gameState->player_decks["player$playerNum"];
+
+        $playerTakerCardsCount = $playerTaker->count;
+        $playerTakerCards = $playerTaker->cards;
+
+        $countCardsInPile = $gameState->pile->count;
+        $CardsInPile = $gameState->pile->cards;
+
+        $newPlayerTakerCardsCount = $countCardsInPile + $playerTakerCardsCount;
+        $newPlayerTakerCards = array_merge($CardsInPile, $playerTakerCards);
+
+        $gameState->player_decks["player$playerNum"]->count[$newPlayerTakerCardsCount];
+        $gameState->player_decks["player$playerNum"]->cards[$newPlayerTakerCards];
+
+        $gameState->pile = [ //reset de la pila
+            'count' => 0,
+            'cards' => [],
+            'last_played_cards' => [],
+            'called_rank' => '0',
+        ];
+
+        $jsonGameState = json_encode($gameState);
+
+        $result = $this->updateGameState($gameId, $jsonGameState);
+
+        if ($result == "Succes in updating the gameState") {
+            return "Taken cards succesfuly";
+        } else {
+            return "Error at tring to take cards";
+        }
+        
+    }
+
+    public function callLie($gameId) {
+        $user = Auth::user();
+        $gameState = $this->getDecodedGameStateById($gameId);
+        
+
+        $players = $gameState->players;
+        $totalPlayers = count($players);
+        $currentPlayerTurn = $gameState->current_player_turn;
+        $lastPlayedCards = $gameState->last_played_cards;
+        $calledRank = $gameState->called_rank;
+
+        $positionOfCurrentPlayer = array_search($currentPlayerTurn, $players);
+        if ($positionOfCurrentPlayer == 0) {
+            $positionOfLastPlayerTurn = $totalPlayers - 1;
+        }
+        $positionOfLastPlayerTurn = $positionOfCurrentPlayer - 1;
+        $lastPlayer = $players[$positionOfLastPlayerTurn];
+
+        $lie = false;
+        foreach ($lastPlayedCards as $card) {
+            $currentCard = Card::find($card);
+            $rankOfCard = $currentCard->rank;
+            if ($rankOfCard != $calledRank) {
+                $lie = true;
+            }
+        }
+
+        if ($lie == false) {
+            $gameState = $this->takeCards($gameId, $user);
+        } else {
+            $gameState = $this->takeCards($gameId, $lastPlayer);
+        }
+
+        //cridar la funcio getDecodedGameStateById
+        //cridar la funcio update gameState
+
+        // return $gameState;
+    }
+    /*
     pile:{
         count:3,
         cards:[1,3,5,7],
         last_played_cards:[5,7],
         last_called_rank:[10],
-    },
-
-    player_decks:[
-        'player1' => ['count' => 0, 'cards' => []],
-        'player2' => ['count' => 0, 'cards' => []],
-        'player3' => ['count' => 0, 'cards' => []],
-        'player4' => ['count' => 0, 'cards' => []],
-        'player5' => ['count' => 0, 'cards' => []],
-        'player6' => ['count' => 0, 'cards' => []],
-    ],
-
-    players:[5,3,8,2,1,10],
-    
-
-    current_player_turn: 2,
+        },
+        
+        player_decks:[
+            'player1' => ['count' => 0, 'cards' => []],
+            'player2' => ['count' => 0, 'cards' => []],
+            'player3' => ['count' => 0, 'cards' => []],
+            'player4' => ['count' => 0, 'cards' => []],
+            'player5' => ['count' => 0, 'cards' => []],
+            'player6' => ['count' => 0, 'cards' => []],
+            ],
+            
+            players:[5,3,8,2,1,10],
+            
+            
+            current_player_turn: 2,
+    }
+    */
 }
-*/
