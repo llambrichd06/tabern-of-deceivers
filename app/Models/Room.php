0d336a94324853391a60cb4a7bc328cd9c041d7c
llambrichd06 <llambrichd06@gmail.com>
2026-04-26 23:15:14 +0200

    TOD-10 RoomFactory is used when executing all seeders

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Events\MessageSent;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -10,6 +11,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Room extends Model
 {
+    use HasFactory;
+    
     protected $fillable = [
         'room_code', //Code for the users to join the room
         'state', //3 states lobby, on_going, completed
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,9 +19,10 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'surname1' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$FuO/F1CVchYC63xyF6/EBuV42LNviwpJdtI0mIkOCEJuR/d0xjg2a', // password for default users is 12345678
             'remember_token' => Str::random(10),
         ];
     }
